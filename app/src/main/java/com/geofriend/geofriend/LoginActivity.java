5bbe5538bb1167022f2143ea761cc1d79dbe2bb4
TysonMoonie <tysonshane97@gmail.com>
2020-10-08 23:23:29 -0700

    Added a conditional if (user!=null) around sendEmailVerification to deal with null pointer exceptions.

--- app/src/main/java/com/geofriend/geofriend/LoginActivity.java
+++ app/src/main/java/com/geofriend/geofriend/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.geofriend.geofriend;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.geofriend.geofriend.databinding.ActivityLoginBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -33,20 +35,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Loads activity_login.xml Layout file
-        setContentView(R.layout.activity_login);
 
         //Initialize Firebase Authentication
         mAuth = FirebaseAuth.getInstance();
 
         //INSTANTIATING DATABINDING
-        mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        //Loads activity_login.xml Layout file
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
         //SETTING ON CLICK LISTENERS
         mBinding.emailSignInButton.setOnClickListener(this);
         mBinding.emailCreateAccountButton.setOnClickListener(this);
         mBinding.signOutButton.setOnClickListener(this);
-        mBinding.verifyEmailButton.setOnClickListener(this);
+        mBinding.exploreButton.setOnClickListener(this);
         mBinding.reloadButton.setOnClickListener(this);
 
     }
@@ -54,7 +55,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onStart() {
         super.onStart();
-        //Check if the user is signed in
+        //Loads User information into the UI
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
@@ -92,18 +93,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void updateUI(FirebaseUser user) {
-
-        user = mAuth.getCurrentUser();
-
         //hideProgressBar();
         if (user != null) {
-            mBinding.status.setText("Signed In.");
-            mBinding.detail.setText("GeoFriend");
+            mBinding.status.setText(R.string.signed_in);
+            mBinding.detail.setText(R.string.app_name);
 
             mBinding.emailPasswordButtons.setVisibility(View.GONE);
             mBinding.emailPasswordFields.setVisibility(View.GONE);
             mBinding.signedInButtons.setVisibility(View.VISIBLE);
-
 
         } else {
             mBinding.status.setText(R.string.signed_out);
@@ -117,33 +114,35 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private void sendEmailVerification() {
         // Disable button
-        mBinding.verifyEmailButton.setEnabled(false);
+        mBinding.exploreButton.setEnabled(false);
 
         // Send verification email
         // [START send_email_verification]
         final FirebaseUser user = mAuth.getCurrentUser();
-        user.sendEmailVerification()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // [START_EXCLUDE]
-                        // Re-enable button
-                        mBinding.verifyEmailButton.setEnabled(true);
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // [START_EXCLUDE]
+                            // Re-enable button
+                            mBinding.exploreButton.setEnabled(true);
 
-                        if (task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this,
-                                    "Verification email sent to " + user.getEmail(),
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            Log.e("TAG", "sendEmailVerification", task.getException());
-                            Toast.makeText(LoginActivity.this,
-                                    "Failed to send verification email.",
-                                    Toast.LENGTH_SHORT).show();
+                            if (task.isSuccessful()) {
+
+                                Toast.makeText(LoginActivity.this, "Verification email sent to " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                            } else {
+
+                                Log.e("TAG", "sendEmailVerification", task.getException());
+                                Toast.makeText(LoginActivity.this, "Failed to send verification email.", Toast.LENGTH_SHORT).show();
+
+                            }
                         }
-                        // [END_EXCLUDE]
-                    }
-                });
-        // [END send_email_verification]
+                    });
+        }
+        else
+            Toast.makeText(LoginActivity.this, "There is no user to send verification to.", Toast.LENGTH_LONG).show();
     }
 
     private void createAccount(String email, String password) {
@@ -162,27 +161,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("TAG", "createUserWithEmail:success");
-                            Toast.makeText(LoginActivity.this, "User Creation Successful. Firebase Updated.",
-                                    Toast.LENGTH_LONG).show();
+
+                            Toast.makeText(LoginActivity.this, "User Creation Successful. Firebase Updated.", Toast.LENGTH_LONG).show();
+
                             FirebaseUser user = mAuth.getCurrentUser();
                             sendEmailVerification();
+
                             updateUI(user);
                         } else {
+
                             // If sign in fails, display a message to the user.
                             Log.w("TAG", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "User Creation Failed.",
-                                    Toast.LENGTH_LONG).show();
+
+                            Toast.makeText(LoginActivity.this, "User Creation Failed.", Toast.LENGTH_LONG).show();
+
                             updateUI(null);
                         }
-
-                        // [START_EXCLUDE]
-                        //hideProgressBar();
-                        // [END_EXCLUDE]
                     }
                 });
-        // [END create_user_with_email]
     }
 
     private void signIn(String email, String password) {
@@ -194,6 +193,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         EditText et1 = findViewById(R.id.fieldPassword);
         password = et1.getText().toString();
 
+
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -201,29 +201,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         if (task.isSuccessful() && mAuth.getCurrentUser().isEmailVerified()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("TAG", "signInWithEmail:success");
-                            Toast.makeText(LoginActivity.this, "Authentication Successful. Signed in.",
-                                    Toast.LENGTH_LONG).show();
+
+                            //Toast.makeText(LoginActivity.this, "Authentication Successful. Signed in.", Toast.LENGTH_LONG).show();
 
                             updateUI(mAuth.getCurrentUser());
-
 
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("TAG", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_LONG).show();
-                            updateUI(null);
-                            // [START_EXCLUDE]
-                            //checkForMultiFactorFailure(task.getException());
-                            // [END_EXCLUDE]
-                        }
 
-                        // [START_EXCLUDE]
+                            //Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+
+                            updateUI(null);
+
+                        }
                         if (!task.isSuccessful()) {
                             mBinding.status.setText(R.string.auth_failed);
                         }
-
-                        // [END_EXCLUDE]
                     }
                 });
         // [END sign_in_with_email]
@@ -266,8 +260,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             signIn(email, password);
         } else if (i == R.id.signOutButton) {
             signOut();
-        } else if (i == R.id.verifyEmailButton) {
-            sendEmailVerification();
+        } else if (i == R.id.exploreButton) {
+            Intent toExplore = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(toExplore);
         } else if (i == R.id.reloadButton) {
             reload();
         }
