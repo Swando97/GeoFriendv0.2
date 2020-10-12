a94b829b0b21330a551b987998e382c669934535
EdwardYeung6 <57809982+EdwardYeung6@users.noreply.github.com>
2020-10-11 18:22:48 -0700

    Add files via upload

---
+++ app/src/main/java/com/geofriend/geofriend/Trip.java
@@ -0,0 +1,119 @@
+package com.geofriend.geofriend;
+
+public class Trip {
+    private int maximun=20;                             //maximun landmark can include
+    private int Count=0;
+    private String Name;
+    private String Creator;
+    private String Intro;
+    private LandMark[] LandMark=new LandMark[maximun];
+    private String[] LandMarkComment;
+    private int Code;
+    private int Rating;
+    //cuurent landmark included
+
+    public Trip() {
+    }
+
+    public Trip(int count, String name, String creator, String intro, com.geofriend.geofriend.LandMark[] landMark, String[] landMarkComment, int code, int rating) {
+        Count = count;
+        Name = name;
+        Creator = creator;
+        Intro = intro;
+        LandMark = landMark;
+        LandMarkComment = landMarkComment;
+        Code = code;
+        Rating = rating;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCreator() {
+        return Creator;
+    }
+
+    public void setCreator(String creator) {
+        Creator = creator;
+    }
+
+    public String getIntro() {
+        return Intro;
+    }
+
+    public void setIntro(String intro) {
+        Intro = intro;
+    }
+
+    public com.geofriend.geofriend.LandMark[] getLandMark() {
+        return LandMark;
+    }
+
+    public void setLandMark(com.geofriend.geofriend.LandMark[] landMark) {
+        LandMark = landMark;
+    }
+
+    public int getCode() {
+        return Code;
+    }
+
+    public void setCode(int code) {
+        Code = code;
+    }
+
+    public int getRating() {
+        return Rating;
+    }
+
+    public void setRating(int rating) {
+        Rating = rating;
+    }
+
+    public String[] getLandMarkComment() {
+        return LandMarkComment;
+    }
+
+    public void setLandMarkComment(String[] landMarkComment) {
+        LandMarkComment = landMarkComment;
+    }
+
+    public void setCount(int count) {
+        this.Count = count;
+    }
+
+    //add LandMark
+    public void addLandMark(LandMark l){
+        LandMark[Count]=l;
+        Count++;
+    }
+
+    //remove from database
+    public void removeTrip(){
+
+    }
+
+    //add this object to database(use as create new object)
+    public void addTrip(){
+
+    }
+
+    //update object to database(use as modify exist object)
+    public void updateTrip(int count, String name, String creator, String intro, com.geofriend.geofriend.LandMark[] landMark, String[] landMarkComment, int code, int rating){
+        Trip old=new Trip(Count,Name,Creator,Intro,LandMark,LandMarkComment,Code,Rating);
+        setCount(count);
+        setName(name);
+        setIntro(intro);
+        setLandMark(landMark);
+        setLandMarkComment(landMarkComment);
+        setCode(code);
+        setRating(rating);
+
+        //replace the old object with the new object in database
+        //
+    }
+}
