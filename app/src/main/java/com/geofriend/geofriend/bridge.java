abcb0b4855982fc8fdf38f4b215c06d3fec98d93
EdwardYeung6 <57809982+EdwardYeung6@users.noreply.github.com>
2020-10-15 23:38:22 -0700

    Add files via upload
    bridge to connect database.

---
+++ app/src/main/java/com/geofriend/geofriend/bridge.java
@@ -0,0 +1,68 @@
+package com.geofriend.geofriend;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.view.View;
+
+public class bridge {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public LandMark l1;
+    public Tour t1;
+
+    public void passLandMark(LandMark l1){
+        this.l1=l1;
+    }
+
+    public void passTour(Tour t1){
+        this.t1=t1;
+    }
+
+
+    public void addData(LandMark l1){
+        passLandMark(l1);
+        db.collection("landmark").document(l1.getName()).set(l1);
+    }
+
+    public void addData(Tour t1){
+        passTour(t1);
+        db.collection("tour").document(t1.getName()).set(t1);
+    }
+
+    public LandMark searchLandMarkByName(String s1){
+
+        DocumentReference docRef = db.collection("landmark").document(s1);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                LandMark l2 = documentSnapshot.toObject(LandMark.class);
+                l1=l2;
+            }
+        });
+        return l1;
+    }
+
+
+
+    public Tour searchTourByName(String s1){
+        DocumentReference docRef = db.collection("tour").document(s1);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Tour t2 = documentSnapshot.toObject(Tour.class);
+                t1=t2;
+            }
+        });
+        return t1;
+    }
+
+    public void deleteData(){
+
+    }
+
+    /*public void updateData(){
+
+    }*/
+}
