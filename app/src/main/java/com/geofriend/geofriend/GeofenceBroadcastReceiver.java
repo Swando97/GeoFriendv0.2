0dcdb26279263ea121dc5d91a1fa66a9abf3ae1b
EdwardYeung6 <57809982+EdwardYeung6@users.noreply.github.com>
2020-11-15 20:28:36 -0800

    Update GeofenceBroadcastReceiver.java

--- app/src/main/java/com/geofriend/geofriend/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/geofriend/geofriend/GeofenceBroadcastReceiver.java
@@ -37,7 +37,7 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
         switch (transitionType) {
             case Geofence.GEOFENCE_TRANSITION_ENTER:
-                Toast.makeText(context, "GEOFENCE_TRANSITION_ENTER: "+geofenceList.get(0).getRequestId(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "GEOFENCE_TRANSITION_ENTER: ", Toast.LENGTH_SHORT).show();
                 notificationHelper.sendHighPriorityNotification("GEOFENCE_TRANSITION_ENTER", "", MapsActivity.class);
                 break;
             case Geofence.GEOFENCE_TRANSITION_DWELL:
